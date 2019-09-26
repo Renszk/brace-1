3d29adfea5889f153e2d50778d66207e9cbe30ff
ren-kemuri <ren@kemuri.in>
2019-09-26 11:47:08 +0530

    test

--- index.js
+++ index.js
@@ -9454,6 +9454,7 @@ ace.define("ace/edit_session/folding",["require","exports","module","ace/range",
                     continue;
                 var doc = this.doc;
                 var range = this.getFoldWidgetRange(row);
+                console.log(doc.$lines[row].indexOf('/**'));
                 if ((range && range.isMultiLine()
                     && range.end.row <= endRow
                     && range.start.row >= startRow) || (all_comments && doc.$lines[row].indexOf('/**') > -1)
