619d8bbb6f1b8b36dce6fc796d37e4f473417690
ren-kemuri <ren@kemuri.in>
2019-10-02 23:40:15 +0530

    test

--- index.js
+++ index.js
@@ -9468,7 +9468,7 @@ ace.define("ace/edit_session/folding",["require","exports","module","ace/range",
                     ) {
                         console.log(5);
                         if(!range  || range == undefined){
-                            this.find('*/', {}, true);
+                            console.log(this.find('*/', {}, false));
                             this.position = this.getCursorPosition();
                             console.log(this.getCursorPosition());
                             row = this.position.row;
